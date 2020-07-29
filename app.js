3c34735f30e47e54ef8ac256a233d4c7a2b7a240
Shyamal Jadav <jadavshyamal.js@gmail.com>
2020-07-29 14:11:39 +0530

    added routing testing

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-var add = (x, y) => {
-    return x + y;
-}
+var express = require("express");
+const request = require("request");
 
-module.exports = add;
+var app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).send("I am shyamal");
+})
+
+app.get("/test", (req, res) => {
+    // if(!req.query.id){
+    //     res.status(401).send({"message":"This is an error message"});
+    // }
+    res.status(500).send({"message":"This is error message"});
+})
+
+var server = app.listen(3000, ()=>{
+    console.log("Listening to port " + server.address().port);
+})
+
+module.exports = server;
+
+// var add = (x, y) => {
+//     return x + y;
+// }
+
+// module.exports = add;
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,10 +1,64 @@
-var Add = require("../app");
+var request = require("request");
+const { response } = require("express");
 
-describe("Add functionality", ()=>{
-    it("Calculate x + y = z", ()=>{
-        expect(Add(10, 5)).toEqual(15);
+describe("Server", ()=>{
+    var server;
+    beforeAll(()=>{
+        server = require("../app");
     })
-    it("Calculate x + y = z", ()=>{
-        expect(Add(10, 5)).not.toEqual(15);
+    afterAll(()=>{
+        server.close();
     })
+
+    describe('GET /', () => {
+        var data = {};
+        beforeAll((done)=>{
+            request.get("http://localhost:3000/", (error, response, body)=>{
+                data.status = response.statusCode;
+                data.body = body;
+                done();
+            })
+        })
+
+        it("Status 200", ()=>{
+            expect(data.status).toBe(200);
+        })
+
+        it("Body", ()=>{
+            expect(data.body).toBe("I am shyamal");
+        })
+
+    })
+
+    describe('GET /test', () => {
+        var data = {};
+        beforeAll((done)=>{
+            request.get("http://localhost:3000/test", (error, response, body)=>{
+                data.status = response.statusCode;
+                data.body = JSON.parse(body);
+                done();
+            })
+        })
+
+        it("Status 500", ()=>{
+            expect(data.status).toBe(500);
+        })
+
+        it("Body", ()=>{
+            expect(data.body.message).toBe("This is error message");
+        })
+
+    })
+        
 })
+
+// var Add = require("../app");
+
+// describe("Add functionality", ()=>{
+//     it("Calculate x + y = z", ()=>{
+//         expect(Add(10, 5)).toEqual(15);
+//     })
+//     it("Calculate x + y = z", ()=>{
+//         expect(Add(10, 5)).not.toEqual(15);
+//     })
+// })
